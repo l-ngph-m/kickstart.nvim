1d3ddb71b89e566670dd615860e53169261eec99
Me <75350061+l-ngph-m@users.noreply.github.com>
2026-01-08 09:43:28 +0800

    remove lspconfig.setup per nvim 0.11 docs

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -18,18 +18,6 @@ return {
     },
     config = function()
       local capabilities = require('blink.cmp').get_lsp_capabilities()
-      require('lspconfig').markdown_oxide.setup {
-        -- Ensure that dynamicRegistration is enabled! This allows the LS to take into account actions like the
-        -- Create Unresolved File code action, resolving completions for unindexed code blocks, ...
-        capabilities = vim.tbl_deep_extend('force', capabilities, {
-          workspace = {
-            didChangeWatchedFiles = {
-              dynamicRegistration = true,
-            },
-          },
-        }),
-        on_attach = on_attach, -- configure your on attach config
-      }
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
