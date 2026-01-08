4cf8d45fc92c57e89c18826999c86242ad8c24a3
Me <75350061+l-ngph-m@users.noreply.github.com>
2026-01-08 09:43:53 +0800

    Change tab settings from 2 to 4 spaces

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -12,9 +12,9 @@ vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.autoindent = true
 vim.o.smartindent = true
